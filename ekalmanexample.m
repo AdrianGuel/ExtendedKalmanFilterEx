4b09e4af30a443834f359fe18e9e98ce94c4acc9
A.-J. Guel-Cortez <52336176+AdrianGuel@users.noreply.github.com>
2020-12-07 23:23:54 +0000

    Add files via upload

---
+++ ekalmanexample.m
@@ -0,0 +1,107 @@
+%Extended kalman filter example
+% mass-spring-damper system
+clear all;
+
+%Solution to the model
+t=0:1e-3:15;
+F=zeros(1,length(t));
+%F=0.1*sin(t);
+k=0.5; b=0.7; m=1;
+h=1e-3;
+y=zeros(2,length(t));
+y(:,1)=[1;0.1];
+for j=2:length(t)
+    y(:,j)=y(:,j-1)+h*model(y(:,j-1),F(j-1),m,b,k);
+end
+
+%Kalman filter
+n=5;
+P_previous=50000*eye(n,n);
+Q=zeros(n,n);
+Q(1,1)=5000; Q(2,2)=5000;
+R=0.01; H=[1,0,0,0,0]; V=1;
+W=eye(n,n);
+
+l=1;
+x_previous=[1,0.1,0.5,0.7,1.0];
+x=zeros(l*length(t),n);
+k=2;
+for i=2:l*length(t)
+    x_model=f(x_previous,F(i-1),[0 0 0 0 0]);
+    P_kminus=A(x_model,F(i-1))*P_previous*A(x_model,F(i-1))'+W*Q*W';
+    K_k=P_kminus*H'*((H*P_kminus*H'+V*R*V')^(-1));
+    x_k=x_model+K_k*(y(1,i)+0.01*randn(1,1)-x_model(1));
+    P_k=(eye(n,n)-K_k*H)*P_kminus;
+    x(i,:)=x_k';
+    x_previous=x_k;
+    P_previous=P_k;
+%         k=k+1;
+%     if k==length(t)
+%        k=2;
+%        P_previous(1:3,1:3)=50*eye(3,3);
+%     end
+end
+
+figure
+set(gcf,'color','w');
+subplot(3,2,[1 2])
+plot(t,y(1,:),'b')
+hold on
+plot(t,x(:,1),'--r','LineWidth',2)
+subplot(3,2,3)
+plot(t,y(2,:),'b')
+hold on
+plot(t,x(:,2),'--r','LineWidth',2)
+subplot(3,2,4)
+plot(t,x(:,3))
+subplot(3,2,5)
+plot(t,x(:,4))
+subplot(3,2,6)
+plot(t,x(:,5));
+m_estimated=1/x(end,5);
+k_estimated=m_estimated*x(end,3);
+b_estimated=m_estimated*x(end,4);
+
+y_e=zeros(2,length(t));
+y_e(:,1)=[1;0.1];
+for j=2:length(t)
+    y_e(:,j)=y_e(:,j-1)+h*model(y_e(:,j-1),F(j-1),m_estimated,b_estimated,k_estimated);
+end
+figure
+set(gcf,'color','w');
+plot(t,y(1,:),'b')
+hold on
+plot(t,y_e(1,:),'--r','LineWidth',2)
+RMSE = sqrt(mean((y(1,:) - y_e(1,:)).^2));  % Root Mean Squared Error
+txt = text(10, 0.8, ("RMSE="+RMSE*1d6),'Interpreter','latex');
+txt.FontSize = 10;
+txt = text(10, 0.7, ("$\hat{m}=$"+m_estimated),'Interpreter','latex');
+txt.FontSize = 10;
+txt = text(10, 0.6, ("$\hat{k}=$"+k_estimated),'Interpreter','latex');
+txt.FontSize = 10;
+txt = text(10, 0.5, ("$\hat{b}=$"+b_estimated),'Interpreter','latex');
+txt.FontSize = 10;
+
+function aux=A(x,F)
+T=1e-3;
+aux=[1 T 0 0 0;
+    -T*x(3) 1-T*x(4) -T*x(1) -T*x(2) T*F;
+    0 0 1 0 0;
+    0 0 0 1 0;
+    0 0 0 0 1];
+end
+
+function aux=f(x,F,w)
+T=1e-3;
+aux=[x(1)+T*x(2)+T*w(1);
+    x(2)-T*x(3)*x(1)-T*x(4)*x(2)+T*x(5)*F+T*w(2);
+    x(3)+T*w(3);
+    x(4)+T*w(4);
+    x(5)+T*w(5)];
+end
+
+
+function aux=model(x,F,m,b,k)
+aux=[x(2);
+    -(k/m)*x(1)-(b/m)*x(2)+F];
+end
