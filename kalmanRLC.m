20a45fde6006f906310a550f7f5acaab8f9e41af
A.-J. Guel-Cortez <52336176+AdrianGuel@users.noreply.github.com>
2021-09-26 19:36:02 +0100

    Create kalmanRLC.m

---
+++ kalmanRLC.m
@@ -0,0 +1,56 @@
+%kalman filter example
+%Aplication in a RLC system 
+%Author: Adrian-Josue Guel-Cortez 2021
+
+%Solution to the model
+R=0.5; L=0.7; C=1;
+T=1e-3;
+t=0:T:15;
+A=[-1/(C*R),1/C;-1/L,0];
+B=[0;1/L];
+Vi=zeros(1,length(t));
+y=zeros(2,length(t));
+y(:,1)=[1;0.1];
+for j=2:length(t)
+    y(:,j)=y(:,j-1)+T*model(y(:,j-1),Vi(j-1),R,L,C);
+end
+
+%Kalman filter
+n=2;
+P_previous=5*eye(n,n);
+Q=zeros(n,n);
+Q(1,1)=5; Q(2,2)=5;
+R=0.001; H=[1,0]; 
+
+x_previous=[1;0.1];
+x=zeros(n,length(t));
+for k=2:length(t)
+    x_model=A*x_previous+B*Vi(k);
+    P_kminus=A*P_previous*A'+Q;
+    K_k=P_kminus*H'*((H*P_kminus*H'+R)^(-1));
+    x_k=x_model+K_k*(y(1,k)+0.01*randn(1,1)-H*x_model);
+    P_k=(eye(n,n)-K_k*H)*P_kminus;
+    x(:,k)=x_k;
+    x_previous=x_k;
+    P_previous=P_k;
+end
+
+figure
+set(gcf,'color','w');
+subplot(2,1,1)
+plot(t,y(1,:),'b')
+hold on
+plot(t,x(1,:),'r')
+xlabel('t')
+ylabel('V_c')
+subplot(2,1,2)
+plot(t,y(2,:))
+hold on
+plot(t,x(2,:),'r')
+xlabel('t')
+ylabel('i_L')
+
+function aux=model(x,Vi,R,L,C)
+aux=[-x(1)/(C*R)+x(2)/C;
+    -x(1)/L+Vi/L];
+end
